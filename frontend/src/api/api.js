fbd6f279202cebfb47cc7577cf49cdeabd752b46
chshin0225 <chshin0225@gmail.com>
2021-03-29 16:46:36 +0900

    Edit api url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,5 @@
 export default {
-    URL: 'http://localhost:8000',
+    URL: 'http://localhost:8000/api',
     ROUTES: {
         hello: '/api1/hello/',
         hello2: '/api2/secondhello/',
--- swaggertest/swaggertest/urls.py
+++ swaggertest/swaggertest/urls.py
@@ -40,6 +40,6 @@ urlpatterns = [
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path('api1/', include('api1.urls')),
-    path('api2/', include('api2.urls')),
+    path('api/api1/', include('api1.urls')),
+    path('api/api2/', include('api2.urls')),
 ]
