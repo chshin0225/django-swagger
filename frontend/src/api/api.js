44eeb3127332aa99c48f9d6d7c969640223f74f9
chshin0225 <chshin0225@gmail.com>
2021-03-30 14:40:05 +0900

    Change request URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,5 @@
 export default {
-    URL: 'http://localhost:80/api',
+    URL: 'http://t4coach34.p.ssafy.io/api',
     ROUTES: {
         hello: '/api1/hello/',
         hello2: '/api2/secondhello/',
